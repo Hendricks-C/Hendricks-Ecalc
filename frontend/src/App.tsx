57fe5273612c20c6e8c7fcaa70a2c04bbbb27823
Julian Lim Jun Ren <limjunre@bu.edu>
2025-01-25 15:27:18 -0500

    basic page routing for login and signup has been setup using react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,27 @@
-
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Login from './pages/login.tsx'
+import Register from './pages/register.tsx'
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    
+    <BrowserRouter>
+      <h1>Test</h1>
+      <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/register">Register</Link></li>
+          </ul>
+      </nav>
+    
+      <Routes>
+        {/* <Route exact path="/" element={Home} /> */}
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+      </Routes>
+    </BrowserRouter>
+    
   )
 }
 
