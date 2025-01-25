508653e21c4421cd9bc11f1fb6d53947c09298b2
Julian Lim Jun Ren <limjunre@bu.edu>
2025-01-25 15:51:26 -0500

    implemented basic navbar component and re-organised app.jsx to include it

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,13 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/login.tsx'
 import Register from './pages/register.tsx'
+import Navbar from './components/navbar.tsx'
 function App() {
 
   return (
     
     <BrowserRouter>
-      <h1>Test</h1>
-      <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/register">Register</Link></li>
-          </ul>
-      </nav>
+      <Navbar />
     
       <Routes>
         {/* <Route exact path="/" element={Home} /> */}
---
+++ frontend/src/components/navbar.tsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+    return (
+        <nav className="flex justify-between items-center flex-row rounded-lg border border-gray-300 bg-white p-4 m-4">
+                <div className="flex">
+                    <Link to="/" className='text-lg'>Hendricks Foundation</Link>
+                </div>
+                <div className="flex flex-row gap-4">
+                    <Link to="/login" className='no-underline hover:underline'>Login</Link>
+                    <Link to="/register" className='no-underline hover:underline'>Register</Link>
+                </div>
+        </nav>
+    )  
+}
+
+export default Navbar
