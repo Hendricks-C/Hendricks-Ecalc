f4102272b72758c3f88a94944717ce05de027eab
Julian Lim Jun Ren <limjunre@bu.edu>
2025-01-25 12:31:13 -0500

    modified form input css

--- frontend/src/components/loginBox.tsx
+++ frontend/src/components/loginBox.tsx
@@ -13,7 +13,7 @@ function LoginBox() {
         <div className="flex flex-col items-center">
             <h1>Information</h1>
             <h2>Fill in the fields below</h2>
-            <div className="p-10 border border-gray-300 rounded-md">
+            <div className="p-10 border border-gray-300 rounded-md bg-opacity-10 bg-gray-100">
                 <form onSubmit={handleSubmit} className="flex flex-col gap-4">
                     <div>
                         <label className="flex">Email:</label>
@@ -23,7 +23,7 @@ function LoginBox() {
                         value={email}
                         onChange={e => setEmail(e.target.value)}
                         required
-                        className="border border-gray-300 rounded-md p-2 placeholder-gray-400 focus:outline-none focus:ring-2"
+                        className="border border-gray-300 rounded-md p-2 placeholder-gray-400 focus:outline-none focus:ring-2 bg-white"
                         />
                     </div>
                     <div>
@@ -33,7 +33,7 @@ function LoginBox() {
                         placeholder="Optional"
                         value={company}
                         onChange={e => setCompany(e.target.value)}
-                        className="border border-gray-300 rounded-md p-2 placeholder-gray-400 focus:outline-none focus:ring-2"
+                        className="border border-gray-300 rounded-md p-2 placeholder-gray-400 focus:outline-none focus:ring-2 bg-white"
                         />
                     </div>
                     <div>
@@ -44,7 +44,7 @@ function LoginBox() {
                         value={password}
                         onChange={e => setPassword(e.target.value)}
                         required
-                        className="border border-gray-300 rounded-md p-2 placeholder-gray-400 focus:outline-none focus:ring-2"
+                        className="border border-gray-300 rounded-md p-2 placeholder-gray-400 focus:outline-none focus:ring-2 bg-white"
                         />
                     </div>
                     <div className="flex flex-col justify-center">
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Login from './pages/login.tsx'
+import Register from './pages/register.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
