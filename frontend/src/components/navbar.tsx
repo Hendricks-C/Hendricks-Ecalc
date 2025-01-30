1bf6407a085090b66375df69b0c7d75f3c31e056
Julian Lim <limjunre@bu.edu>
2025-01-30 00:41:58 -0500

    added signout button

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,6 +1,16 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import supabase from '../utils/supabase'
 
 function Navbar() {
+    const navigate = useNavigate()
+    const handleClick = async () => {
+        const { error } = await supabase.auth.signOut()
+        if (error) {
+            console.error('Error logging out:', error.message)
+            return
+        }
+        navigate('/login')
+    }
     return (
         <nav className="flex justify-between items-center flex-row rounded-lg border border-gray-300 bg-white p-4 m-4">
                 <div className="flex">
@@ -11,6 +21,7 @@ function Navbar() {
                     <Link to="/contact" className='no-underline hover:underline'>Contact</Link>
                     <Link to="/login" className='no-underline hover:underline'>Login</Link>
                     <Link to="/register" className='no-underline hover:underline'>Register</Link>
+                    <button onClick={handleClick} className="border items-center rounded-md">Sign Out</button>
                 </div>
         </nav>
     )  
