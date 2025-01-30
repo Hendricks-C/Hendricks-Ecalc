a5a392c3a76ea4f9a2a08ba2f144eac6f89ee950
Julian Lim <limjunre@bu.edu>
2025-01-30 01:23:58 -0500

    minor css change, centered navbar items vertically

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -13,10 +13,10 @@ function Navbar() {
     }
     return (
         <nav className="flex justify-between items-center flex-row rounded-lg border border-gray-300 bg-white p-4 m-4">
-                <div className="flex">
+                <div className="flex items-center">
                     <Link to="/" className='text-lg'>Hendricks Foundation</Link>
                 </div>
-                <div className="flex flex-row gap-4">
+                <div className="flex flex-row gap-4 items-center">
                     <Link to="/about" className='no-underline hover:underline'>About Us</Link>
                     <Link to="/contact" className='no-underline hover:underline'>Contact</Link>
                     <Link to="/login" className='no-underline hover:underline'>Login</Link>
