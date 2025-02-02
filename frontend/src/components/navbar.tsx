6dd827f6caf60abf002215db2a3188c5bf89235a
Senahilal <senab@bu.edu>
2025-02-02 16:10:04 -0500

    Updated navbar to show/hide login and register links based on user session

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,14 +1,40 @@
 import { Link, useNavigate } from 'react-router-dom'
 import supabase from '../utils/supabase'
+import { useEffect, useState } from 'react';
 
 function Navbar() {
+
+    const [user, setUser] = useState<any>(null)
     const navigate = useNavigate()
+
+    // Check for existing user session
+    useEffect(() => {
+        
+        async function checkUser() {
+            const { data, error } = await supabase.auth.getUser();
+            if (!error) {
+                setUser(data?.user || null);
+            }
+        }
+
+        checkUser();
+
+        // Listen for auth state changes
+        const { data: authListener } = supabase.auth.onAuthStateChange((_, session) => {
+            setUser(session?.user || null);
+        });
+
+        //Cleans up the listener when the component unmounts.
+        return () => authListener.subscription.unsubscribe();
+    }, []);
+
     const handleClick = async () => {
         const { error } = await supabase.auth.signOut()
         if (error) {
             console.error('Error logging out:', error.message)
             return
         }
+        setUser(null);
         navigate('/login')
     }
     return (
@@ -19,9 +45,15 @@ function Navbar() {
                 <div className="flex flex-row gap-4 items-center">
                     <Link to="/about" className='no-underline hover:underline'>About Us</Link>
                     <Link to="/contact" className='no-underline hover:underline'>Contact</Link>
-                    <Link to="/login" className='no-underline hover:underline'>Login</Link>
-                    <Link to="/register" className='no-underline hover:underline'>Register</Link>
-                    <button onClick={handleClick} className="border p-1 items-center rounded-md bg-green-300 hover:bg-green-200 cursor-pointer active:bg-green-600">Sign Out</button>
+                    {!user ? (
+                    <>
+                        <Link to="/login" className="no-underline hover:underline">Login</Link>
+                        <Link to="/register" className="no-underline hover:underline">Register</Link>
+                    </>
+                    ) : (
+                        <button onClick={handleClick} className="border p-1 items-center rounded-md bg-green-300 hover:bg-green-200 cursor-pointer active:bg-green-600">Sign Out</button>
+                    )}
+                    
                 </div>
         </nav>
     )  
