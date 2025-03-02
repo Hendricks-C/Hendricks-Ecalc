80fbf0d5031996fb4d5bf924559cf86abbb098fe
Senahilal <senab@bu.edu>
2025-03-02 14:03:37 -0500

    Implemented Cloudflare Turnstile CAPTCHA to login and register pages

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,12 +1,18 @@
 import { Link, useNavigate } from 'react-router-dom'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import supabase from '../utils/supabase.ts'
 import { AuthResponse } from '@supabase/supabase-js'
+import { Turnstile } from '@marsidev/react-turnstile'
 
 function Login() {
   const [email, setEmail] = useState<string>('')
   const [company, setCompany] = useState<string>('')
   const [password, setPassword] = useState<string>('')
+  const [captchaToken, setCaptchaToken] = useState<string | null>(null);
+
+  const [captchaKey, setCaptchaKey] = useState(0);
+
+  const [captchaError, setCaptchaError] = useState<string | null>(null);
   const navigate = useNavigate()
 
   //checking for existing user session
@@ -16,21 +22,34 @@ function Login() {
         navigate("/welcome");
       }
     });
-  
+
     return () => authListener.subscription.unsubscribe(); //clean up
   }, [navigate]);
 
   //login user
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
+
+    if (!captchaToken) {
+      setCaptchaError('Please complete the captcha');
+      return;
+    }
+
+    setCaptchaError(null);
+
     const { data, error }: AuthResponse = await supabase.auth.signInWithPassword({ //call signin function from supabase
       email,
       password,
+      options: { captchaToken },
     })
 
+    //Reset the token after submission
+    setCaptchaToken(null);
+    
     // error handling
     if (error) {
       alert(error.message)
+      setCaptchaKey((prev) => prev + 1); // Trigger re-render Turnstile component
       return
     }
 
@@ -40,59 +59,67 @@ function Login() {
   }
 
   return (
-    <> 
+    <>
       <div className='flex flex-row justify-evenly items-center'>
         <div>
           <img src="../assets/react.svg" alt="Hendricks Foundation" />
         </div>
         <div className="flex flex-col items-center">
-            <h1>Login</h1>
-            <h2>Fill in the fields below</h2>
-            <div className="p-10 border border-gray-300 rounded-2xl bg-opacity-10 bg-gray-100">
-                <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-                    <div>
-                        <label className="flex">Email:</label>
-                        <input
-                        type="text"
-                        placeholder="email@example.com"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                        required
-                        className="w-full border border-gray-300 rounded-md p-2 placeholder-gray-400 focus:outline-none focus:ring-2 bg-white"
-                        />
-                    </div>
-                    <div>
-                        <label className="flex">Company:</label>
-                        <input
-                        type="text"
-                        placeholder="Optional"
-                        value={company}
-                        onChange={e => setCompany(e.target.value)}
-                        className="w-full border border-gray-300 rounded-md p-2 placeholder-gray-400 focus:outline-none focus:ring-2 bg-white"
-                        />
-                    </div>
-                    <div>
-                        <label className="flex">Password:</label>
-                        <input
-                        type="password"
-                        placeholder="**********"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                        required
-                        className="w-full border border-gray-300 rounded-md p-2 placeholder-gray-400 focus:outline-none focus:ring-2 bg-white"
-                        />
-                    </div>
-                    <div className="flex flex-col justify-center gap-2 items-center">
-                        <button className="border p-1 w-1/2 items-center rounded-md bg-green-300 hover:bg-green-200 cursor-pointer active:bg-green-600" type="submit">Login</button>
-                        <button className="border p-1 w-1/2 items-center rounded-md bg-green-300 hover:bg-green-200 cursor-pointer active:bg-green-600" type="button">Skip</button>
-                    </div>
-                    <div>
-                        <p>Don't have an account? <Link to="/register" className='no-underline hover:underline text-blue-500'>Create one now.</Link></p>
-                        <p>Forgot your password? <Link to="/forgot-password" className='no-underline hover:underline text-blue-500'>Reset it here.</Link></p>
-                    </div>
-                </form>
-                
-            </div>
+          <h1>Login</h1>
+          <h2>Fill in the fields below</h2>
+          <div className="p-10 border border-gray-300 rounded-2xl bg-opacity-10 bg-gray-100">
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+              <div>
+                <label className="flex">Email:</label>
+                <input
+                  type="text"
+                  placeholder="email@example.com"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                  required
+                  className="w-full border border-gray-300 rounded-md p-2 placeholder-gray-400 focus:outline-none focus:ring-2 bg-white"
+                />
+              </div>
+              <div>
+                <label className="flex">Company:</label>
+                <input
+                  type="text"
+                  placeholder="Optional"
+                  value={company}
+                  onChange={e => setCompany(e.target.value)}
+                  className="w-full border border-gray-300 rounded-md p-2 placeholder-gray-400 focus:outline-none focus:ring-2 bg-white"
+                />
+              </div>
+              <div>
+                <label className="flex">Password:</label>
+                <input
+                  type="password"
+                  placeholder="**********"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                  required
+                  className="w-full border border-gray-300 rounded-md p-2 placeholder-gray-400 focus:outline-none focus:ring-2 bg-white"
+                />
+              </div>
+              <div className="flex flex-col justify-center gap-2 items-center">
+                <button className="border p-1 w-1/2 items-center rounded-md bg-green-300 hover:bg-green-200 cursor-pointer active:bg-green-600" type="submit">Login</button>
+                <button className="border p-1 w-1/2 items-center rounded-md bg-green-300 hover:bg-green-200 cursor-pointer active:bg-green-600" type="button">Skip</button>
+              </div>
+              <div >
+                <Turnstile
+                  key={captchaKey} // Change this key to reset
+                  siteKey={import.meta.env.VITE_TURNSTILE_SITE_KEY}
+                  onSuccess={(token) => { setCaptchaToken(token) }} />
+
+                {captchaError && <p style={{ color: 'red', fontSize: '0.75rem' }}>{captchaError}</p>}
+              </div>
+              <div>
+                <p>Don't have an account? <Link to="/register" className='no-underline hover:underline text-blue-500'>Create one now.</Link></p>
+                <p>Forgot your password? <Link to="/forgot-password" className='no-underline hover:underline text-blue-500'>Reset it here.</Link></p>
+              </div>
+            </form>
+
+          </div>
         </div>
       </div>
     </>
--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -2,11 +2,18 @@ import { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import supabase from '../utils/supabase.ts'
 import { AuthResponse } from "@supabase/supabase-js";
+import { Turnstile } from '@marsidev/react-turnstile'
+
 
 function Register() {
   const [email, setEmail] = useState<string>('')
   const [company, setCompany] = useState<string>('')
   const [password, setPassword] = useState<string>('')
+  const [captchaToken, setCaptchaToken] = useState<string | null>(null);
+
+  const [captchaKey, setCaptchaKey] = useState(0);
+
+  const [captchaError, setCaptchaError] = useState<string | null>(null);
   const navigate = useNavigate() //used to redirect to different page
 
   //checking for existing user session
@@ -23,23 +30,35 @@ function Register() {
   //register user
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault();
+    if (!captchaToken) {
+      setCaptchaError('Please complete the captcha');
+      return;
+    }
+    setCaptchaError(null);
+
     const { data, error }: AuthResponse = await supabase.auth.signUp({ //call signup function from supabase
       email,
       password,
       options: {
         emailRedirectTo: 'http://localhost:5173/welcome', //temporary welcome link
-        data: { company: company.trim() } // Send company info in metadata
+        data: { company: company.trim() }, // Send company info in metadata
+        captchaToken
       },
     })
+
+    //Reset the token after submission
+    setCaptchaToken(null);
 
     //error handling
     if (error) {
       console.error('Error signing up:', error.message)
       alert(error.message)
+      setCaptchaKey((prev) => prev + 1); // Trigger re-render Turnstile component
       return
     } else if (data.user?.identities?.length === 0) {
       console.error('User already Exists')
       alert('User already Exists')
+      setCaptchaKey((prev) => prev + 1);
       return
     }
 
@@ -91,6 +110,14 @@ function Register() {
                   required
                   className="w-full border border-gray-300 rounded-md p-2 placeholder-gray-400 focus:outline-none focus:ring-2 bg-white"
                 />
+              </div>
+              <div >
+                <Turnstile
+                  key={captchaKey} // Change this key to reset
+                  siteKey={import.meta.env.VITE_TURNSTILE_SITE_KEY}
+                  onSuccess={(token) => { setCaptchaToken(token) }} />
+
+                {captchaError && <p style={{ color: 'red', fontSize: '0.75rem' }}>{captchaError}</p>}
               </div>
               <div className="flex flex-col justify-center gap-2 items-center">
                 <button className="border p-1 w-1/2 items-center rounded-md bg-green-300 cursor-pointer hover:bg-green-200 active:bg-green-600" type="submit">Sign Up</button>
