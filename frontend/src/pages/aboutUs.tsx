edaaf92f407cb5f58515de080f069ba8c103dd0c
Julian Lim Jun Ren <limjunre@bu.edu>
2025-03-29 14:33:01 -0400

    setup aboutUs skeleton

---
+++ frontend/src/pages/aboutUs.tsx
@@ -0,0 +1,22 @@
+
+export default function AboutUs(){
+    return (
+        <>
+            <div className="flex flex-col justify-center items-center border border-gray-300 rounded-lg m-4 p-4">
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+            
+        </>
+    )
+}
